0ed02b324fef682dcb1722749dd6a3f0f3033ac1
shogenji <ruirui@ruirui.jp>
2023-06-07 16:40:30 +0900

    更新 hanya.ts

--- hanya.ts
+++ hanya.ts
@@ -2,10 +2,12 @@
  * Test for Building Extensions.
  */
 //% color=190 weight=100 icon="\uf0c3" block="TestBlocks"
+//% groups="['Ha', 'Hi']"
 namespace hanya {
     //% blockId=show_heart
     //% block="show heart | for $duration x 0.1 seconds"
     //% duration.min=0 duration.max=1000 duration.defl=1
+    //% group="Ha"
     export function showHeart(duration: number): void {
         basic.showIcon(IconNames.Heart)
         basic.pause(duration * 100)
@@ -17,6 +19,7 @@ namespace hanya {
     //% block="Show small heart | for $duration"
     //% duration.min=0 duration.max=100000 duration.defl=1000
     //% duration.shadow="timePicker"
+    //% group="Hi"
     export function showSmallHeart(duration: number): void {
         basic.showIcon(IconNames.SmallHeart)
         basic.pause(duration)
